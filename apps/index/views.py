1de2bc0a95bcdd27f5b3e856d2681e77bb03ccef
Toni Isotalo <toni.isotalo@hotmail.com>
2021-09-07 17:05:11 +0300

    Initial commit

---
+++ apps/index/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout as _logout
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def logout(request):
+    _logout(request)
+    return redirect("/")
+
+def landingpage(request):
+    return render(request, "index/index.html")
+
+def signin(request):
+    data = {"error": ""}
+    if(request.method == "POST"):
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
+        if(user is not None):
+            login(request, user)
+            if(request.GET.get("next")):
+                return redirect(request.GET.get("next"))
+            return redirect("/")
+        else:
+            data["error"] = "User not found"
+
+    return render(request, "index/signin.html", data)
+
+def register(request):
+    data = {"errors":{}}
+
+    if(request.method == "POST"):
+        #form request
+        firstname = request.POST.get("firstname")
+        data["firstname"] = firstname
+        lastname = request.POST.get("lastname")
+        data["lastname"] = lastname
+        email = request.POST.get("email")
+        data["email"] = email
+        password = request.POST.get("password")
+        data["password"] = password
+
+        success = 0
+        if(len(firstname) < 2):
+            data["errors"]["firstname"] = "Firstname is too short"
+        elif(len(firstname) > 30):
+            data["errors"]["firstname"] = "Firstname is too long"
+        else:
+            success = success + 1
+
+        if(len(lastname) < 2):
+            data["errors"]["lastname"] = "Lastname is too short"
+        elif(len(lastname) > 30):
+            data["errors"]["lastname"] = "Lastname is too long"
+        else:
+            success = success + 1
+
+        validEmail = False
+        try:
+            validate_email(email)
+            validEmail = True
+        except:
+            data["errors"]["email"] = "Invalid email"
+
+        try:
+            if(validEmail):
+                User.objects.get(email=email)
+                data["errors"]["email"] = "Email is already in use"
+        except:
+            success = success + 1
+
+        if(len(password) < 3):
+            data["errors"]["password"] = "Password is too short"
+        elif(len(password) > 50):
+            data["errors"]["password"] = "Password is too long"
+        else:
+            success = success + 1
+        
+        if(success == 4):
+            #All fields are valid
+            user = User(first_name = firstname, last_name=lastname, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect("/")
+
+    return render(request, "index/register.html", data)
---
+++ lib/backends.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+
+class Authentication(BaseBackend):
+    
+    def authenticate(self, request, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if(check_password(password, user.password)):
+                return user
+        except User.DoesNotExist:
+            return None
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
