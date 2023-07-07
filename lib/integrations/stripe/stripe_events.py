0f96e26656b958936ad18b2e709458e45453a1d9
Toni Isotalo <toni.isotalo@hotmail.com>
2023-07-07 22:07:26 +0300

    Making new subscription ready

--- lib/integrations/stripe/stripe_events.py
+++ lib/integrations/stripe/stripe_events.py
@@ -12,6 +12,7 @@ def new_subscription(data):
 
 def invoice_paid(data):
     payment = stripe_event_objects.Invoice(data)
+    handle_new_invoice(payment)
 
 def get_payment_object(ob):
     pmt = Payment(user=ob.buyer, product=ob.product, price=ob.price)
@@ -32,10 +33,31 @@ def get_purchase_skeleton(ob):
         payment=get_payment_object(ob),
     )
 
+def finish_new_subscription(inv, sub):
+    sub.payment.invoice = inv.invoice
+    sub.start_date = inv.start_date
+    sub.end_date = inv.end_date
+    sub.status = parameters.Stripe.Purchase.Status.paid
+    sub.payment.save()
+    sub.save()
+
+def handle_new_invoice(inv):
+    sub_id = inv.subscription_id
+    subs_exist = Purchase.objects.filter(stripe_sub_id=sub_id)
+    if(subs_exist.count() == 1):
+        sub = subs_exist.first()
+        if(sub.status == parameters.Stripe.Purchase.Status.waiting_payment and sub.buyer.id == inv.buyer.id):
+            finish_new_subscription(inv, sub)
+            return
+    
+    #New period for existing subscription
+
+
 def handle_new_subscription(sub):
     purchase = get_purchase_skeleton(sub)
     purchase.stripe_sub_id = sub.subscription_id
     purchase.status = parameters.Stripe.Purchase.Status.waiting_payment
     purchase.payment.save()
     purchase.save()
+    #The subscription will be finished when the related invoice comes
     
