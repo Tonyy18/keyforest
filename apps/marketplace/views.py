a3cd678a2fe2cf1e8a97a75f363e659e7d287434
Toni Isotalo <toni.isotalo@evitec.com>
2023-07-24 04:34:21 +0300

    (accountPage) Verify stripe webhook

--- apps/marketplace/views.py
+++ apps/marketplace/views.py
@@ -52,21 +52,23 @@ def accountPage(request):
     subs = {}
     sub_payment_ids = []
     results = []
-    for sub in subscriptions:
-        if(sub.period_id not in subs):
-            subs[sub.period_id] = {"childs": []}
-        if("header" not in subs[sub.period_id]):
-            subs[sub.period_id]["header"] = sub.payment
-        else:
-            subs[sub.period_id]["childs"].append(sub.payment)
-        sub_payment_ids.append(sub.payment.id)
+    if(subscriptions):
+        for sub in subscriptions:
+            if(sub.period_id not in subs):
+                subs[sub.period_id] = {"childs": []}
+            if("header" not in subs[sub.period_id]):
+                subs[sub.period_id]["header"] = sub.payment
+            else:
+                subs[sub.period_id]["childs"].append(sub.payment)
+            sub_payment_ids.append(sub.payment.id)
 
-    for sub in subs:
-        results.append(subs[sub])
-
-    for pay in payments:
-        if(pay.id not in sub_payment_ids):
-            results.append({"header": pay})
+        for sub in subs:
+            results.append(subs[sub])
+    
+    if(payments):
+        for pay in payments:
+            if(pay.id not in sub_payment_ids):
+                results.append({"header": pay})
 
     return render(request, "marketplace/account_page.html", {
         "purchases": purchases,
